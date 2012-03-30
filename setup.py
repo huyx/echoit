669cb451d93b26251918e18df78a4f0b42febbe1
huyx <ycyuxin@gmail.com>
2012-03-30 16:42:46 +0800

    来自 http://svn.wordaligned.org/svn/etc/echo，参见 http://wordaligned.org/articles/echo

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='echo',
+      version='0.2',
+      py_modules=['echo'],
+      description='Tracing function calls using Python decorators',
+      author='Thomas Guest',
+      author_email='tag@wordaligned.org',
+      url='http://wordaligned.org/articles/echo',
+      )
+
